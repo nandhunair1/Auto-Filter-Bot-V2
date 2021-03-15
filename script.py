10411efe952922ad1e04c43528ae8acd2cb35f31
nandhunair1 <77907590+nandhunair1@users.noreply.github.com>
2021-03-15 22:04:58 +0530

    Update script.py

--- script.py
+++ script.py
@@ -53,17 +53,17 @@ No need add each filter again!
 Bot will automatically search for your files and give links to that!
 
 
-<b>© @TroJanzHEX</b>"""
+<b>© @MrC_VENOM</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @MrC_VENOM
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>Support Group :</b> <a href='https://t.me/tvseriezzz'>Group Link</a> 
 
 """
